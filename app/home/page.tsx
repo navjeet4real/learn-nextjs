4ef9dfbae717b3708ed6882371cd96d3caf8f513
navjeet4real <navjeetkajal.2594@gmail.com>
2024-04-12 16:38:38 +0530

    test branch msg

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -5,6 +5,7 @@ export default function Page () {
         <div>
             <h1>Home</h1>
             <h2>Payments</h2>
+            <h2>Test Message</h2> 
             <EmbeddedCheckoutButton />
         </div>
     );
